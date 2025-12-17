3f06aa15b52c8d337e2a4f179482b4a7313b6bf1
siyukatu <siyukatu@gmail.com>
2025-12-17 16:26:19 +0900

    Update _middleware.ts

--- src/functions/_middleware.ts
+++ src/functions/_middleware.ts
@@ -5,9 +5,7 @@ export async function onRequest(context) {
   const assetResponse = await context.env.ASSETS.fetch(context.request);
   const res = await assetResponse.text().replace("{{time}}", Date.now());
   return new Response(res, {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/html; charset=UTF-8',
-    },
+    status: assetResponse.status,
+    headers: assetResponse.headers,
   });
 }
