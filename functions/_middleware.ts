0608a937d3c5a9c4ef981670420d303a9afbdd90
siyukatu <siyukatu@gmail.com>
2025-12-17 16:39:06 +0900

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,10 +2,14 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   const path = url.pathname;
 
-  const assetResponse = await context.env.ASSETS.fetch(context.request);
-  const res = (await assetResponse.text()).replace("{{time}}", Date.now());
-  return new Response(res, {
-    status: assetResponse.status,
-    headers: assetResponse.headers,
-  });
+  if (path === "/time.css") {
+    return new Response(res, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/css",
+        "Cache-Control": "no-store",
+      },
+    });
+  }
+  return await context.next();
 }
